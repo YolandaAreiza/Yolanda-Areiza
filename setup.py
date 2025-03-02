4fb77d8fa2c52b0c38161a87e26029b141a9a477
YolandaAreiza <yolanda.areiza@est.iudigital.edu.co>
2025-03-02 20:49:16 +0000

    carga inicial

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Yolanda Areiza",
+    author_email="yolanda.areiza@est.iudigital.edu.co",
+    description="Actividad programacion oob",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "request",
+        "matplotlib"
+    ]
+    
+    
+)
